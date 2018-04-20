b36cd89ae23902aedbb797c6f7cf8ab852021d18
wondervictor <victorchanchina@gmail.com>
2018-04-20 10:15:29 +0800

    add files

--- environment/knobs.py
+++ environment/knobs.py
@@ -44,6 +44,33 @@ KNOB_DETAILS = {
 
 }
 
+
+UNIFIED_KNOBS = {
+    'skip_name_resolve': ['enum', ['OFF', 'ON'], None],
+    'table_open_cache': ['integer', [1, 524288, 2000], None],
+    'max_connections': ['integer', [1, 100000, 151], None],
+    # innodb_buffer_pool_size unit: MB
+    'innodb_buffer_pool_size': ['integer_MB', [1, 32768, 16384], None],
+    # WARN: innodb_buffer_pool_size 80% of memory
+    'innodb_buffer_pool_instances': ['integer', [1, 64, 8], None],
+    'innodb_log_files_in_group': ['integer', [2, 100, 2], None],
+    # innodb_log_file_size unit: MB
+    'innodb_log_file_size': ['integer_MB', [1, 5424, 48], ['innodb_log_files_in_group']],
+    'innodb_purge_threads': ['integer', [1, 32, 1], None],
+    'innodb_read_io_threads': ['integer', [1, 64, 4], None],
+    'innodb_write_io_threads': ['integer', [1, 64, 4], None],
+    'innodb_file_per_table': ['enum', ['OFF', 'ON'], None],
+    'binlog_checksum': ['enum', ['NONE', 'CRC32'], None],
+    # binlog_cache_size unit: MB
+    'binlog_cache_size': ['integer_MB', [32768, 0.03125], None],
+    # max_binlog_cache_size unit: GB
+    'max_binlog_cache_size': ['integer_GB', [4096, 17179869183, 17179869183], None],
+    # max_binlog_size unit: MB
+    'max_binlog_size': ['integer_MB', [0.00390625, 1024, 1024], None],
+    'binlog_format': ['enum', ['ROW', 'STATEMENT', 'MIXED'], None],
+
+}
+
 num_knobs = len(KNOBS)
 
 
@@ -121,7 +148,32 @@ def gen_discrete(action, knobs):
 
 
 def gen_continuous(action):
-    pass
+    result = {}
+    for idx in xrange(num_knobs):
+        name = KNOBS[idx]
+        value = UNIFIED_KNOBS[name]
+        knob_type = value[0]
+        knob_value = value[1]
+        if knob_type == 'integer_MB':
+            max_val = knob_value[1]
+            eval_value = int(max_val * action[idx])*1024*1024
+        elif knob_type == 'integer_GB':
+            max_val = knob_value[1]
+            eval_value = int(max_val * action[idx])*1024*1024*1024
+        elif knob_type == 'integer':
+            max_val = knob_value[1]
+            eval_value = int(max_val * action[idx])
+        else:
+            enum_size = len(knob_value)
+            enum_index = int(enum_size * action[idx])
+            eval_value = knob_value[enum_index]
+
+        if name == 'innodb_log_file_size':
+            max_val = 16 * 1024 / result['innodb_log_files_in_group']
+            eval_value = int(max_val * action[idx])*1024*1024
+        result[name] = eval_value
+
+    return result
 
 
 def save_knobs(knob, metrics, knob_file):
