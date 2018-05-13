c28960cac156ef49482a1520bbd6a15dd3725cee
wondervictor <victorchanchina@gmail.com>
2018-05-13 16:37:22 +0800

    fix server bugs

--- server/server.py
+++ server/server.py
@@ -15,12 +15,20 @@ docker = False
 
 
 def get_state():
+    check_start()
     m = os.popen('service mysql status')
     s = m.readlines()[2]
     s = s.split(':')[1].replace(' ', '').split('(')[0]
     if s == 'failed':
         return -1
     return 1
+
+
+def check_start():
+    a = sudo_exec('sudo tail -1 /var/log/mysql/ubunturmw.err', '123456')
+    a = a.strip('\n\r')
+    if a.find('/var/lib/mysql/ubunturmw.pid ended') != -1:
+        sudo_exec('sudo service mysql start', '123456')
 
 
 def sudo_exec(cmdline, passwd):
@@ -72,7 +80,7 @@ def write_cnf_file(configs):
     Args:
         configs: str, Formatted MySQL Parameters, e.g. "--binlog_size=xxx"
     """
-    cnf_file = '/etc/mysql/conf.d/mysql.cnf'
+    cnf_file = '/etc/mysql/my.cnf'
     config_parser = CP.ConfigParser()
     sudo_exec('sudo chmod 777 %s' % cnf_file, '123456')
     time.sleep(2)
